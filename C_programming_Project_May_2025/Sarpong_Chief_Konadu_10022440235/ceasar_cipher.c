540e1c71c29fe324b63c5edaf51a3edde46c3905
Mawuli-lab <mawuli.prebbie@gmail.com>
2025-08-16 21:55:57 +0000

    Update ceasar_cipher.c

--- C_programming_Project_May_2025/Sarpong_Chief_Konadu_10022440235/ceasar_cipher.c
+++ C_programming_Project_May_2025/Sarpong_Chief_Konadu_10022440235/ceasar_cipher.c
@@ -1,33 +1,29 @@
 #include <stdio.h>
-#include <string.h>
 #include <ctype.h>
+#include <string.h>
 
-int main(){
-    char text[100];
-    int key;
-
-    printf("Enter the key:\n");
-    printf("->");
-    scanf("%d", &key);
-    getchar();
-
-    printf("Enter the plain text:\n");
-    printf("->");
-    fgets(text, 100, stdin);
-
-    for(int i = 0; text[i] != '\0'; i++){
-        char cipher = text[i];
-
-        if(isupper(cipher)){
-            cipher = ((cipher - 'A' + key) % 26) + 'A';
+int main(void) {
+    char plaintext[1000], ciphertext[1000];
+    int key = 3; // Example key - could be made user input
+    
+    printf("plaintext: ");
+    fgets(plaintext, sizeof(plaintext), stdin);
+    
+    // Remove newline character
+    plaintext[strcspn(plaintext, "\n")] = '\0';
+    
+    for (int i = 0; i < strlen(plaintext); i++) {
+        if (isupper(plaintext[i])) {
+            ciphertext[i] = ((plaintext[i] - 'A' + key) % 26) + 'A';
+        } else if (islower(plaintext[i])) {
+            ciphertext[i] = ((plaintext[i] - 'a' + key) % 26) + 'a';
+        } else {
+            ciphertext[i] = plaintext[i]; // Non-alphabetic characters remain unchanged
         }
-
-        else if (islower(cipher)) {
-            cipher = ((cipher - 'a' + key) % 26) + 'a';
-        }
-
-        printf("%c", cipher);
     }
-
+    
+    ciphertext[strlen(plaintext)] = '\0';
+    
+    printf("ciphertext: %s\n", ciphertext);
     return 0;
 }
