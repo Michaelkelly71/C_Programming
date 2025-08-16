4c8a1ddf26fa983217bdab023cab1cee27e51a5e
Mawuli-lab <mawuli.prebbie@gmail.com>
2025-08-16 21:55:07 +0000

    Update dna_compatability.c

--- C_programming_Project_May_2025/Sarpong_Chief_Konadu_10022440235/dna_compatability.c
+++ C_programming_Project_May_2025/Sarpong_Chief_Konadu_10022440235/dna_compatability.c
@@ -1,40 +1,45 @@
 #include <stdio.h>
-#include <string.h>
 #include <ctype.h>
+#include <string.h>
+#include <math.h>
 
-int main() {
-    char dna[1000], complement[1000];
-
-    printf("Enter DNA sequence: ");
-    scanf("%s", dna); 
-
-    int length = strlen(dna);
-
-    for (int i = 0; i < length; i++) {
-        char base = toupper(dna[i]); 
-
-        switch (base) {
-            case 'A':
-                complement[i] = 'T';
-                break;
-            case 'T':
-                complement[i] = 'A';
-                break;
-            case 'C':
-                complement[i] = 'G';
-                break;
-            case 'G':
-                complement[i] = 'C';
-                break;
-            default:
-                printf("\nInvalid base '%c' found in DNA sequence!\n", base);
-                return 1; 
+int main(void) {
+    char text[1000];
+    int letters = 0, words = 0, sentences = 0;
+    
+    printf("Text: ");
+    fgets(text, sizeof(text), stdin);
+    
+    for (int i = 0; i < strlen(text); i++) {
+        if (isalpha(text[i])) {
+            letters++;
+        }
+        
+        if (text[i] == ' ' && i > 0 && text[i-1] != ' ') {
+            words++;
+        }
+        
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?') {
+            sentences++;
         }
     }
-
-    complement[length] = '\0'; 
-
-    printf("Complementary DNA: %s\n", complement);
-
+    
+    if (strlen(text) > 0 && text[strlen(text)-1] != ' ') {
+        words++;
+    }
+    
+    float L = (letters / (float)words) * 100;
+    float S = (sentences / (float)words) * 100;
+    
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
+    
+    if (index < 1) {
+        printf("Before Grade 1\n");
+    } else if (index >= 16) {
+        printf("Grade 16+\n");
+    } else {
+        printf("Grade %d\n", index);
+    }
+    
     return 0;
 }
