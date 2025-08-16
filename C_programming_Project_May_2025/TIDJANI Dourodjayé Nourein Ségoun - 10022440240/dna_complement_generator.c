df5aeeb167597a08412855f0c0ddbde66028a7e2
Nourgoun <tidjani.segoun@acity.edu.gh>
2025-08-16 16:46:06 +0000

    Add files via upload

---
+++ C_programming_Project_May_2025/TIDJANI Dourodjayé Nourein Ségoun - 10022440240/dna_complement_generator.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+int main(void){
+    char dna[1000];
+    printf("Enter a DNA sequence: ");
+    scanf("%s", dna);
+    int length = strlen(dna);
+    for (int i=0; i<length; i++){
+        switch (dna[i]){
+            case 'A': dna[i] = 'T'; break;
+            case 'T': dna[i] = 'A'; break;
+            case 'C': dna[i] = 'G'; break;
+            case 'G': dna[i] = 'C'; break;
+            case 'a': dna[i] = 't'; break;
+            case 't': dna[i] = 'a'; break;
+            case 'c': dna[i] = 'g'; break;
+            case 'g': dna[i] = 'c'; break;
+            default:  dna[i] = '?';
+        }
+    }
+    printf("Complement: %s\n", dna);
+    return 0;
+}
---
+++ C_programming_Project_May_2025/TIDJANI Dourodjayé Nourein Ségoun - 10022440240/readability_analyzer.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<math.h>
+#include<string.h>
+int main(){
+    char text[1000];
+    printf("Enter you text: ");
+    fgets(text,sizeof(text),stdin);
+    int length=strlen(text), letters=0, words=1, sentences=0 ;
+    for(int i=0; i<length; i++){
+        if(isalpha(text[i])){
+            letters++;
+        }if(text[i]==' '){
+            words++;
+        }if(text[i]=='.'|| text[i]=="!"|| text[i]=="?"){
+            sentences++;
+        }
+    }
+    float L = ((float)letters / words) * 100;
+    float S = ((float)sentences / words) * 100;
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
+    if (index < 1) {
+        printf("Before Grade 1\n");
+    } else if (index >= 16) {
+        printf("Grade 16+\n");
+    } else {
+        printf("Grade %d\n", index);
+    } 
+    return 0;
+}
