4221b831e8b986a5de9b9eaf0af3049658755ae0
Mawuli-lab <mawuli.prebbie@gmail.com>
2025-08-16 22:00:37 +0000

    Update readability_analyzer.c

--- C_programming_Project_May_2025/Sarpong_Chief_Konadu_10022440235/readability_analyzer.c
+++ C_programming_Project_May_2025/Sarpong_Chief_Konadu_10022440235/readability_analyzer.c
@@ -3,35 +3,44 @@
 #include <string.h>
 #include <math.h>
 
-int main() {
+int main(void) {
     char text[1000];
+    int letters = 0, words = 0, sentences = 0;
+    
     printf("Text: ");
     fgets(text, sizeof(text), stdin);
-
-    int letters = 0, words = 1, sentences = 0;
-
-    for (int i = 0; text[i] != '\0'; i++) {
+    
+    for (int i = 0; i < strlen(text); i++) {
         if (isalpha(text[i])) {
             letters++;
-        } else if (text[i] == ' ') {
+        }
+        
+        if (text[i] == ' ' && i > 0 && text[i-1] != ' ') {
             words++;
-        } else if (text[i] == '.' || text[i] == '!' || text[i] == '?') {
+        }
+        
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?') {
             sentences++;
         }
     }
+    
 
-    float L = ((float)letters / words) * 100;
-    float S = ((float)sentences / words) * 100;
-    float index = 0.0588 * L - 0.296 * S - 15.8;
-    int grade = round(index);
-
-    if (grade < 1) {
+    if (strlen(text) > 0 && text[strlen(text)-1] != ' ') {
+        words++;
+    }
+    
+    float L = (letters / (float)words) * 100;
+    float S = (sentences / (float)words) * 100;
+    
+    int index = round(0.0588 * L - 0.296 * S - 15.8)
+    
+    if (index < 1) {
         printf("Before Grade 1\n");
-    } else if (grade >= 16) {
+    } else if (index >= 16) {
         printf("Grade 16+\n");
     } else {
-        printf("Grade %d\n", grade);
+        printf("Grade %d\n", index);
     }
-
+    
     return 0;
 }
