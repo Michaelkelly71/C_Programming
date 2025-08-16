8bf21a76644d11b4f3e113f4aa2906558a1a61e7
Jesse Antwi <jesseaantwi@gmail.com>
2025-08-16 19:54:09 +0000

    Jesse Amponsah Antwi-10022440077

---
+++ C_programming_Project_May_2025/Jesse Amponsah Antwi-10022440077/DNA_generator.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char dna[1000];
+    printf("Enter a DNA sequence: ");
+    fgets(dna, sizeof(dna), stdin);
+
+    // Remove newline character if present
+    size_t len = strlen(dna);
+    if (dna[len - 1] == '\n') {
+        dna[len - 1] = '\0';
+    }
+
+    printf("Complement: ");
+    for (int i = 0; dna[i] != '\0'; i++) {
+        switch (dna[i]) {
+            case 'A':
+                printf("T");
+                break;
+            case 'T':
+                printf("A");
+                break;
+            case 'C':
+                printf("G");
+                break;
+            case 'G':
+                printf("C");
+                break;
+            case 'a':
+                printf("t");
+                break;
+            case 't':
+                printf("a");
+                break;
+            case 'c':
+                printf("g");
+                break;
+            case 'g':
+                printf("c");
+                break;
+            default:
+                printf("?"); 
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
