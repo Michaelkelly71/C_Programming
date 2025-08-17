f7355010090c5855eaa5666fc39795c1c2c0824a
athan999 <nana.osafo-kissi@acity.edu.gh>
2025-08-17 23:25:04 +0000

    Add files via upload

---
+++ C_programming_Project_May_2025/NANA YAW BOYE OSAFO-KISSI--10022440228/DNA_Complement_Generator.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char dna[1000];
+    printf("Enter a DNA sequence: ");
+    scanf("%s", dna);
+
+    printf("Complement: ");
+    for (int i = 0; dna[i] != '\0'; i++) {
+        char base = dna[i];
+        switch (base) {
+            case 'A': printf("T"); break;
+            case 'T': printf("A"); break;
+            case 'C': printf("G"); break;
+            case 'G': printf("C"); break;
+            case 'a': printf("t"); break;
+            case 't': printf("a"); break;
+            case 'c': printf("g"); break;
+            case 'g': printf("c"); break;
+            default:  printf("%c", base);
+        }
+    }
+    printf("\n");
+    return 0;
+}
---
+++ C_programming_Project_May_2025/NANA YAW BOYE OSAFO-KISSI--10022440228/Readability_Analyzer.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+
+int main() {
+    char text[1000];
+    printf("Text: ");
+    fgets(text, sizeof(text), stdin);
+
+    int letters = 0, words = 1, sentences = 0;
+
+    for (int i = 0; text[i] != '\0'; i++) {
+        if (isalpha(text[i])) {
+            letters++;
+        }
+        if (text[i] == ' ') {
+            words++;
+        }
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?') {
+            sentences++;
+        }
+    }
+
+    // Calculate L and S
+    float L = ((float)letters / words) * 100;
+    float S = ((float)sentences / words) * 100;
+
+    // Coleman–Liau Index
+    float index = 0.0588 * L - 0.296 * S - 15.8;
+    int grade = (int)round(index);
+
+    if (grade < 1) {
+        printf("Before Grade 1\n");
+    } else if (grade >= 16) {
+        printf("Grade 16+\n");
+    } else {
+        printf("Grade %d\n", grade);
+    }
+
+    return 0;
+}
