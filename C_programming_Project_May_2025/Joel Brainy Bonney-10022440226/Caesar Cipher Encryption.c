6b274481e489a24699b806581febdd2bf97d15e9
heisjoel <169050991+heisjoel@users.noreply.github.com>
2025-08-17 18:38:58 +0000

    DONE

---
+++ C_programming_Project_May_2025/Joel Brainy Bonney-10022440226/Caesar Cipher Encryption.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main() {
+    char text[1000];
+    int key;
+
+    printf("plaintext: ");
+    fgets(text, sizeof(text), stdin);
+
+    printf("Enter key: ");
+    scanf("%d", &key);
+
+    printf("ciphertext: ");
+    for (int i = 0; text[i] != '\0'; i++) {
+        if (isalpha(text[i])) {
+            char base = isupper(text[i]) ? 'A' : 'a';
+            printf("%c", (text[i] - base + key) % 26 + base);
+        } else {
+            printf("%c", text[i]);
+        }
+    }
+    printf("\n");
+    return 0;
+}
